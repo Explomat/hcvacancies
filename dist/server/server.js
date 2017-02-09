e48f0d2d182eb4c13d0b4526a417778fc5836264
Savva <cabehok@inbox.ru>
2017-02-09 15:30:26 +0300

    cahnges

--- dist/server/server.js
+++ dist/server/server.js
@@ -37,7 +37,7 @@ function _toJSON(obj){
 	return tools.object_to_text(obj, 'json');
 }
 
-function _vacanciesCount(connection, search, status, limitRows){
+function _vacanciesCount(connection, search, states, limitRows){
 	var query = "
 		select 
 		COUNT(*)/"+limitRows+" pages_count,
@@ -45,42 +45,47 @@ function _vacanciesCount(connection, search, status, limitRows){
 	from 
 		vacancies 
 	where 
-		vacancies.state_id in ('" + status + "') and 
+		vacancies.state_id in ('" + states + "') and 
 		vacancies.name LIKE '%" + search + "%'";
 	var recordSet = connection.Execute(query);
 	return __fetchData(recordSet)[0];
 }
 
-function _vacancies(connection, search, page, status, order, limitRows){
+function _vacancies(connection, userHexId, search, page, states, order, limitRows){
 	var orders = order.split(':');
 	var query = "
-		select vs.* from (select 
-		top " + limitRows + " CONVERT(BIGINT, v.id) as id,
-		REPLACE(v.title, '\"', '') as title,
-		v.status,
-		v.date,
-		v.candidates_num as candidates_count
-	from (
-		select ROW_NUMBER() OVER(ORDER BY vacancies.name) rowNum, 
-		vacancies.id,
-		vacancies.name as title,
-		vacancies.state_id as status,
-		vacancies.start_date as date
+	select
+		top 15
+		v.id,
+		REPLACE(v.name, '\"', '') as name,
+		v.state_id,
+		v.start_date,
+		v.candidates_count
 		
-		from vacancies 
+		from (
+			select ROW_NUMBER() OVER(ORDER BY vacancies." + orders[0] + " " + orders[1] + ") rowNum, 
+			vacancies.id,
+			vacancies.candidates_num as candidates_count,
+			vacancies.name,
+			vacancies.state_id,
+			vacancies.start_date
+			
+			from vacancies 
+			inner join persons p on p.id = vacancies.rr_persons.value('(rr_persons/rr_person/person_id)[1]','varchar(max)')
+			where 
+				vacancies.name LIKE '%" + search + "%'
+				and vacancies.state_id in ('" + states + "')
+				and p.eid is not null
+				and p.eid <> ''
+				and p.eid = '" + userHexId + "'
+		) v
 		where 
-		vacancies.name LIKE '%"+search+"%' and 
-		vacancies.state_id in ('" + status + "')
-	) v
-	where 
-		v.rowNum > " + page * limitRows + ") vs 
-	order by vs." + orders[0] + " " + orders[1];
-	
+			v.rowNum > " + page * limitRows;
 	var recordSet = connection.Execute(query);
 	return __fetchData(recordSet);
 }
 
-function _vacancyStatuses(connection){
+function _vacancyStates(connection){
 	var query = "
 		select vs.id, vs.name,	
 		case
@@ -97,14 +102,14 @@ function _vacancyStatuses(connection){
 
 function _vacancy(connection, vacancyId){
 	var vacancyQuery = "
-		select v.id, v.name as title, v.state_id as status, v.start_date as date
+		select v.id, v.name, v.state_id, v.start_date
 		from vacancies v 
 		where v.id = " + vacancyId;
 	var vacancyRecordSet = connection.Execute(vacancyQuery);
 	var vacancyData = __fetchData(vacancyRecordSet)[0];
 	if (vacancyData != undefined){
 		var candidatesQuery = "
-			select c.id, c.fullname, c.state_id, c.attachments, 
+			select c.id, c.fullname, c.state_id, 
 			(select COUNT(e.id) from events e where e.candidate_id = c.id) as comments_count 
 			from candidates c 
 			where c.id in 
@@ -114,17 +119,17 @@ function _vacancy(connection, vacancyId){
 		var candidatesData = __fetchData(candidatesRecordSet);
 		return {
 			id: vacancyData.id,
-			title: vacancyData.title,
-			status: vacancyData.status,
-			statuses: _vacancyStatuses(connection),
-			date: vacancyData.date,
+			name: vacancyData.name,
+			state_id: vacancyData.state_id,
+			states: _vacancyStates(connection),
+			start_date: vacancyData.start_date,
 			candidates: candidatesData
 		}
 	}
 	return null;
 }
 
-function _candidateStatuses(connection){
+function _candidateStates(connection){
 	var query = "
 		select cs.id, cs.name,
 		case
@@ -172,7 +177,7 @@ function _candidate(connection, vacancyId, candidateId){
 	}
 	
 	var candidateQuery = "
-		select c.id, c.fullname, c.state_id as status, c.attachments 
+		select c.id, c.fullname, c.state_id, c.attachments 
 		from candidates c
 		where c.id = " + candidateId;
 	var candidateRecordSet = connection.Execute(candidateQuery);
@@ -183,7 +188,7 @@ function _candidate(connection, vacancyId, candidateId){
 			case 
 				when e.occurrence_id = '' then e.type_id
 				when e.occurrence_id <> '' then e.type_id + ':' + e.occurrence_id
-			end status
+			end state_id
 			from events e
 			inner join users u on u.id = e.user_id
 			inner join candidates c on c.id = e.candidate_id
@@ -191,15 +196,15 @@ function _candidate(connection, vacancyId, candidateId){
 			and e.candidate_id = " + candidateId;
 		var commentsRecordSet = connection.Execute(commentsQuery);
 		var commentsData = __fetchData(commentsRecordSet);
-		var candidateStatuses = _candidateStatuses(connection);
+		var candidateStates = _candidateStates(connection);
 		return {
 			id: candidateData.id,
 			fullname: candidateData.fullname,
-			status: candidateData.status,
+			state: candidateData.state_id,
 			attachment_id: _candidateAttachmentId(candidateData.attachments),
 			comments: commentsData,
-			statuses: candidateStatuses,
-			bossStatus: 'test'
+			states: candidateStates,
+			bossState: 'test'
 		}
 	}
 	return null;
@@ -211,63 +216,67 @@ function _bossCommentForCandidate(){
 
 function getVacancies(queryObjects){
 	var DEFAULT_LIMIT_ROWS = 15;
+	var connection = null;
 	try {
-		var connection = __connect();
-		var vacancyStatuses = _vacancyStatuses(connection);
+		connection = __connect();
+		var vacancyStates = _vacancyStates(connection);
 		
 		var search = queryObjects.HasProperty('search') ? queryObjects.search : '';
-		var page = queryObjects.HasProperty('page') ? queryObjects.page : 1;
-		var status = 
-			queryObjects.HasProperty('status') ? 
-			(queryObjects.status == 'all' ?
-			ArrayMerge(vacancyStatuses, 'This.id', '\',\'') :
-			queryObjects.status) :
-			ArrayMerge(vacancyStatuses, 'This.id', '\',\'');
+		var page = queryObjects.HasProperty('page') ? queryObjects.page : 0;
+		var states = 
+			queryObjects.HasProperty('states') ? 
+			(queryObjects.states == 'all' ?
+			ArrayMerge(vacancyStates, 'This.id', '\',\'') :
+			queryObjects.states) :
+			ArrayMerge(vacancyStates, 'This.id', '\',\'');
 		
-		var order = queryObjects.HasProperty('order') ? queryObjects.order : 'title:asc';
+		var order = queryObjects.HasProperty('order') ? queryObjects.order : 'name:asc';
 		var limitRows = queryObjects.HasProperty('limit_rows') ? queryObjects.limit_rows : DEFAULT_LIMIT_ROWS;
 		
-		var vacanciesCount = _vacanciesCount(connection, search, status, limitRows);
+		var vacanciesCount = _vacanciesCount(connection, search, states, limitRows);
 		//var vc = _vacancies(connection, search, page, status, orderedByTitle, orderedByStatus, limitRows);
 		//alert('vacancies: ' + _toJSON(vc));
 		alert(_toJSON({
-			vacancies: _vacancies(connection, search, page, status, order, limitRows),
+			vacancies: _vacancies(connection, '0x36726E0B10161F00', search, page, states, order, limitRows),
 			pages_count: vacanciesCount.pages_count,
 			count: vacanciesCount.count,
-			statuses: vacancyStatuses
+			states: vacancyStates
 		}));
 	} catch (e){
-		
+		if (connection != null){
+			__closeConnect(connection);
+		}
 		alert(e);
-	} finally{
-		__closeConnect(connection);
 	}
 }
 
 function getVacancy(queryObjects){
 	var vacancyId = queryObjects.HasProperty('vacancy_id') ? queryObjects.vacancy_id : null;
+	var connection = null;
 	
 	try {
 		if (vacancyId == null){
 			throw "Неверные входные данные!";
 		}
-		var connection = __connect();
+		connection = __connect();
 		var vacancy = _vacancy(connection, vacancyId);
 		alert(_toJSON(vacancy));
 	} catch (e){
+		if (connection != null){
+			__closeConnect(connection);
+		}
 		alert(e);
-	} finally {
-		__closeConnect();
 	}
 }
 
 function getCandidateResume(queryObjects){
 	var attachmentId = queryObjects.HasProperty('attachment_id') ? queryObjects.attachment_id : null;
+	var connection = null;
 	try {
 		if (attachmentId == null){
 			throw "Неверные входные данные!";
 		}
-		var connection = __connect();
+		connection = __connect();
 		var resume = _candidateResume(connection, attachmentId);
 		
 		var fileData = LoadFileData(ROOT_DIRECTORY + '\\'+ curUserID+'\\'+ fileName);
@@ -275,31 +284,35 @@ function getCandidateResume(queryObjects){
 		Request.AddRespHeader("Content-Disposition","attachment; filename=resume.html");
 		return fileData;
 	} catch(e){
+		if (connection != null){
+			__closeConnect(connection);
+		}
 		alert(e);
-	} finally {
-		__closeConnect();
 	}
 }
 
 function getCandidate(queryObjects){
 	var vacancyId = queryObjects.HasProperty('vacancy_id') ? queryObjects.vacancy_id : null;
 	var candidateId = queryObjects.HasProperty('candidate_id') ? queryObjects.candidate_id : null;
+	var connection = null;
 	
 	try {
 		if (vacancyId == null || candidateId == null){
 			throw "Неверные входные данные!";
 		}
-		var connection = __connect();
+		connection = __connect();
 		var candidate = _candidate(connection, vacancyId, candidateId);
 		alert(_toJSON(candidate));
 	} catch (e){
+		if (connection != null){
+			__closeConnect(connection);
+		}
 		alert(e);
-	} finally {
-		__closeConnect();
 	}
 }
 
-/*function updateBossCommentForCandidate(queryObjects){
+function updateBossCommentForCandidate(queryObjects){
+	var connection = null;
 	try {
 		var data = tools.read_object(queryObjects.Body);
 		var vacancyId = data.HasProperty('vacancy_id') ? data.vacancy_id : null;
@@ -309,9 +322,15 @@ function getCandidate(queryObjects){
 		if (vacancyId == null || candidateId == null || comment == null){
 			throw "Неверные входные данные!";
 		}
+		connection = __connect();
+	} catch (e){
+		if (connection != null){
+			__closeConnect(connection);
+		}
+		alert(e);
 	}
-}*/
+}
 
-//getVacancies({});
-//getVacancy({vacancy_id: 20687982340604723});
-//getCandidate({vacancy_id: 5617996101482139474, candidate_id: 1193842844893290933});
+getVacancies({});
+getVacancy({vacancy_id: 20687982340604723});
+getCandidate({vacancy_id: 5617996101482139474, candidate_id: 1193842844893290933});
